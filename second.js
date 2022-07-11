b0e987254f6ef26c01cfef3b4de71e14bf3b6f42
vivian <vivian.v18723@gmail.com>
2022-07-11 12:46:11 +0100

    practice objects

--- second.js
+++ second.js
@@ -1,40 +1,12 @@
-// const countIncluded = (singleNum, arr) => {
-//   let count = 0;
-
-//   for (const el of arr) {
-//     if (el === singleNum) {
-//       count++;
-//     }
-//   }
-
-//   return count;
-// };
-// console.log(countIncluded(1, [2, 1, 2, 3, 1, 1, 1, 4]));
-
-const constGetSmallerCount = (val1, val2, arr) => {
-  let count1 = 0;
-  let count2 = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === val1) {
-      count1++;
-    } else if (arr[i] === val2) {
-      count2++;
-    }
-  }
-  if (count1 < count2) {
-    return val1;
-  } else if (count1 > count2) {
-    return val2;
-  } else {
-    return "same";
-  }
+const chicagoMedCast = {
+  fname: "Robyn",
+  lName: "Charles",
+  age: 31,
+  job: "Epidemiologist",
 };
 
-console.log(constGetSmallerCount(1, 2, [1, 1, 2, 3, 4])); // -> 2
-console.log(constGetSmallerCount(1, 2, [1, 2, 2, 3, 1, 2, 3, 4, 2])); // -> 1
-console.log(constGetSmallerCount(8, 5, [5, 5, 8])); // -> 8
+console.log(chicagoMedCast["fname"]);
+console.log(chicagoMedCast.lName);
 
-// Write a function that takes 3 arguments
-// Two values and an array
-// Return the value that appears fewer times in the array
+chicagoMedCast["age"] = 27;
+console.log(chicagoMedCast);
