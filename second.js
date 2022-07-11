2a55aa52bd83a7e1c9678ff5570f8300897576fb
vivian <vivian.v18723@gmail.com>
2022-07-11 14:23:39 +0100

    solve task

--- second.js
+++ second.js
@@ -1,31 +1,43 @@
-const chicagoMedCast = {
-  fname: "Robyn",
-  lName: "Charles",
-  age: 31,
-  job: "Epidemiologist",
-  "gone or not": "gone",
-};
-
-console.log(chicagoMedCast["fname"]);
-console.log(chicagoMedCast.lName);
-console.log(chicagoMedCast["gone or not"]);
-
-chicagoMedCast["age"] = 27;
-console.log(chicagoMedCast);
-
-const users = [
+const data = [
   {
-    username: "Pete",
+    text: "some text in a string",
+    likes: 3,
+    poster: "a name",
   },
   {
-    username: "Edis",
-    scores: [10, 1, 2, 3],
+    text: "some text in a string",
+    likes: 700,
+    poster: "a name",
   },
   {
-    username: "Holly",
+    text: "some text in a string",
+    likes: 30,
+    poster: "a name",
+  },
+  {
+    text: "some text in a string",
+    likes: 1,
+    poster: "a name",
+  },
+  {
+    text: "some text in a string",
+    likes: 100,
+    poster: "a name",
   },
 ];
 
-console.log(users[1].username);
-console.log(users[1]["username"]);
-console.log(users[1]["scores"][0]);
+const myFunc = (arrOfUserPosts) => {
+  let currentBiggest = 0;
+
+  for (let i = 0; i < arrOfUserPosts.length; i++) {
+    if (arrOfUserPosts[i]["likes"] > currentBiggest) {
+      currentBiggest = arrOfUserPosts[i].likes;
+    }
+  }
+  return currentBiggest;
+};
+
+console.log(myFunc(data));
+
+// take array of post & return username of most liked post
+console.log(myFunc2());
