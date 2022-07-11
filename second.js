4c4a7cb037eb0b52e389441b2b5b2f5afe9be907
vivian <vivian.v18723@gmail.com>
2022-07-11 12:57:31 +0100

    add object practice

--- second.js
+++ second.js
@@ -3,10 +3,29 @@ const chicagoMedCast = {
   lName: "Charles",
   age: 31,
   job: "Epidemiologist",
+  "gone or not": "gone",
 };
 
 console.log(chicagoMedCast["fname"]);
 console.log(chicagoMedCast.lName);
+console.log(chicagoMedCast["gone or not"]);
 
 chicagoMedCast["age"] = 27;
 console.log(chicagoMedCast);
+
+const users = [
+  {
+    username: "Pete",
+  },
+  {
+    username: "Edis",
+    scores: [10, 1, 2, 3],
+  },
+  {
+    username: "Holly",
+  },
+];
+
+console.log(users[1].username);
+console.log(users[1]["username"]);
+console.log(users[1]["scores"][0]);
