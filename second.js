82b14ee96c687ca0ed36706d255f5d49879db5f6
vivian <vivian.v18723@gmail.com>
2022-07-11 11:29:38 +0100

    add function practice

--- second.js
+++ second.js
@@ -1,9 +1,20 @@
-const arr = [1, 2, 3];
+const arr = [100, 90, 2, 45, 66, 0, 1, 2, 3];
 
-console.log(arr);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
+console.log(arr.length);
+console.log(arr.indexOf(3));
 
-arr.push(4);
+for (const item of arr) {
+  console.log("Item: ", item);
+}
+
+arr.sort((a, b) => a - b);
 console.log(arr);
+
+const greaterThan3 = (arr) => {
+  for (const singleArrItem of arr) {
+    if (singleArrItem > 3) {
+      console.log(singleArrItem);
+    }
+  }
+};
+greaterThan3([2, 45, 66, 0, 1, 2, 3]);
